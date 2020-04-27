4f992d4d71cb3fcc9e2ab0fbf09f069bcb4af152
gandh99 <gandh99@hotmail.com>
2020-04-27 17:13:40 +0800

    Initial commit

---
+++ src/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const logger = require('./logger');
+
+module.exports = function (app) {
+  mongoose.connect(
+    app.get('mongodb'),
+    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
+  )
+    .then(conn => console.log(`MongoDB Connected: ${conn.connection.host}`))
+    .catch(err => {
+      logger.error(err);
+      process.exit(1);
+    });
+
+  mongoose.Promise = global.Promise;
+
+  app.set('mongooseClient', mongoose);
+};
