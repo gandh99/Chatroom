c65cf4d6da50b5a87d234052f13b0575c52f5491
gandh99 <gandh99@hotmail.com>
2020-04-28 20:23:34 +0800

    Added test for Header

---
+++ client/src/authentication/Header.test.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import Header from './Header'
+
+describe('Header Component', () => {
+    let wrapper
+    beforeEach(() => {
+      wrapper = shallow(<Header />).childAt(0).dive()
+    })
+
+    it('Should render links', () => {
+        const component = wrapper.find('Link')
+        expect(component.length).toBe(2)
+    })
+
+    it('Should render login and register links', () => {
+        const components = wrapper.find('Link')
+        const loginComponent = components.get(0)
+        const registerComponent = components.get(1)
+
+        expect(loginComponent.props.to).toEqual('/login')
+        expect(registerComponent.props.to).toEqual('/register')
+    })
+})
