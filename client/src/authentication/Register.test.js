1624469d2a90b9a62d0ac468e2b7dc39254f5830
gandh99 <gandh99@hotmail.com>
2020-04-28 23:52:16 +0800

    Added functionality testing for input fields in Register:

--- client/src/authentication/Register.test.js
+++ client/src/authentication/Register.test.js
@@ -13,44 +13,84 @@ jest.mock(`react-redux`, () => ({
 }))
 
 describe('Register Component', () => {
-    let wrapper
-    beforeEach(() => {
-        useDispatch.mockClear()
-        wrapper = shallow(<Register />)
+    describe('Renders', () => {
+        let wrapper
+        beforeEach(() => {
+            useDispatch.mockClear()
+            wrapper = shallow(<Register />)
+        })
+
+        it('Should render header', () => {
+            const component = wrapper.find(Header)
+            expect(component.length).toBe(1)
+        })
+
+        it('Should render banner', () => {
+            const component = wrapper.find(Banner)
+            expect(component.length).toBe(1)
+        })
+
+        it('Should render custom snackbar', () => {
+            const component = wrapper.find(CustomSnackbar)
+            expect(component.length).toBe(1)
+        })
+
+        it('Should render two text fields', () => {
+            const component = wrapper.find(TextField)
+            expect(component.length).toBe(2)
+        })
+
+        it('Should render username text field', () => {
+            const component = wrapper.find('#username')
+            expect(component.length).toBe(1)
+        })
+
+        it('Should render password text field', () => {
+            const component = wrapper.find('#password')
+            expect(component.length).toBe(1)
+        })
+
+        it('Should render submit button', () => {
+            const component = wrapper.find(Button)
+            expect(component.length).toBe(1)
+        })
     })
 
-    it('Should render header', () => {
-        const component = wrapper.find(Header)
-        expect(component.length).toBe(1)
-    })
+    describe('Functionality', () => {
+        let wrapper
+        beforeEach(() => {
+            useDispatch.mockClear()
+            wrapper = shallow(<Register />)
+        })
 
-    it('Should render banner', () => {
-        const component = wrapper.find(Banner)
-        expect(component.length).toBe(1)
-    })
+        // it('Should show error when nothing entered', () => {
+        //     wrapper.find(TextField).at(0).simulate('change', {
+        //         target: { value: 'username' }
+        //     })
+        //     wrapper.update()
+        //     const submitButton = wrapper.find(Button)
+        //     submitButton.simulate('click')
+        //     expect(wrapper.find(TextField).at(0).props().error).toBe(true)
+        // })
 
-    it('Should render custom snackbar', () => {
-        const component = wrapper.find(CustomSnackbar)
-        expect(component.length).toBe(1)
-    })
+        it('Should set the username value on change event', () => {
+            const passwordTextField = wrapper.find(TextField).at(0)
+            passwordTextField.simulate('change', {
+                target: {
+                    value: 'username',
+                },
+            })
+            expect(wrapper.find(TextField).at(0).props().value).toBe('username')
+        })
 
-    it('Should render two text fields', () => {
-        const component = wrapper.find(TextField)
-        expect(component.length).toBe(2)
-    })
-
-    it('Should render username text field', () => {
-        const component = wrapper.find('#username')
-        expect(component.length).toBe(1)
-    })
-
-    it('Should render password text field', () => {
-        const component = wrapper.find('#password')
-        expect(component.length).toBe(1)
-    })
-
-    it('Should render submit button', () => {
-        const component = wrapper.find(Button)
-        expect(component.length).toBe(1)
+        it('Should set the password value on change event', () => {
+            const passwordTextField = wrapper.find(TextField).at(1)
+            passwordTextField.simulate('change', {
+                target: {
+                    value: 'password',
+                },
+            })
+            expect(wrapper.find(TextField).at(1).props().value).toBe('password')
+        })
     })
 })
