6cac59d4760ec122ed1f0902c7bff043fbc75479
gandh99 <gandh99@hotmail.com>
2020-04-29 11:03:44 +0800

    Added UI for home page

--- client/src/home/HomePage.js
+++ client/src/home/HomePage.js
@@ -2,10 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './homepage.css'
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
-// import Header from './Header'
-// import DiaryPage from '../diary/DiaryPage'
-// import FriendsPage from '../friends/FriendsPage'
-// import DrawerMenu from './DrawerMenu'
+import Header from './Header'
 import {
     BrowserRouter as Router,
     Switch,
@@ -13,71 +10,38 @@ import {
 } from "react-router-dom"
 import { PrivateRoute } from '../reusableComponents/PrivateRoute'
 import { useDispatch } from 'react-redux'
-// import { getFriendsAction, getFriendRequestsIssuedToMeAction } from '../redux/actions/friendsActions'
-// import { getDiaryPostsAction, getDiaryPostsSharedWithMeAction } from '../redux/actions/diaryActions'
-// import { getSharedDiaryPostsNotificationsAction, getReceivedFriendRequestsNotificationsAction } from '../redux/actions/notificationsActions'
-// import { initIO } from '../utils/io'
+import TabBar from './TabBar'
 
 export default function HomePage() {
     const classes = useStyles()
     const dispatch = useDispatch()
 
-    // For drawer
-    const [drawerOpen, setDrawerOpen] = useState(false)
-
-    // useEffect(() => {
-    //     // Initiate IO connection with the server
-    //     initIO(dispatch)
-
-    //     // Fetch all required data
-    //     dispatch(getFriendsAction())
-    //     dispatch(getFriendRequestsIssuedToMeAction())
-    //     dispatch(getReceivedFriendRequestsNotificationsAction())
-    //     dispatch(getDiaryPostsAction())
-    //     dispatch(getDiaryPostsSharedWithMeAction())
-    //     dispatch(getSharedDiaryPostsNotificationsAction())
-    // }, [])
-
     return (
-        <>{'Hello world!'}</>
-        // <Router>
-        //     <div className={classes.root}>
-        //         <DrawerMenu drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
-        //         <div className={clsx(classes.contentArea, {
-        //             [classes.contentShift]: drawerOpen,
-        //         })}>
-        //             <Header
-        //                 drawerOpen={drawerOpen}
-        //                 setDrawerOpen={setDrawerOpen}
-        //             />
-        //             <div className={classes.pageArea}>
-        //                 <Switch>
-        //                     <PrivateRoute path='/diary' component={DiaryPage} />
-        //                     <PrivateRoute path='/friends' component={FriendsPage} />
-        //                     <Redirect from='/' to='/diary' />
-        //                 </Switch>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </Router>
+        <Router>
+            <div className={classes.root}>
+                <Header />
+                <div className={classes.pageArea}>
+                    <Switch>
+                        {/* <PrivateRoute path='/diary' component={DiaryPage} />
+                        <PrivateRoute path='/friends' component={FriendsPage} />
+                        <Redirect from='/' to='/diary' /> */}
+                    </Switch>
+                </div>
+                <div className={classes.footer}>
+                    <TabBar />
+                </div>
+            </div>
+        </Router>
     )
 }
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: 'flex',
-        flexDirection: 'row'
     },
-    contentArea: {
-        flexGrow: 1,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-    pageArea: {
+    footer :{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0
     }
 }))
---
+++ client/src/home/TabBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import ForumIcon from '@material-ui/icons/Forum';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PeopleIcon from '@material-ui/icons/People';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+
+export default function TabBar() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Paper square className={classes.root}>
+            <Tabs
+                className={classes.tabs}
+                value={value}
+                onChange={handleChange}
+                variant="fullWidth"
+                indicatorColor="none"
+                textColor="primary"
+                aria-label="icon tabs example"
+            >
+                <Tab icon={<ForumIcon />} aria-label="chats" />
+                <Tab icon={<PeopleIcon />} aria-label="friends" />
+                <Tab icon={<PersonPinIcon />} aria-label="person" />
+            </Tabs>
+        </Paper>
+    )
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: 500,
+    },
+    tabs: {
+        backgroundColor: theme.palette.grey[100]
+    }
+}))
