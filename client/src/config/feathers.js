3a62e65848642d0ec3c5c605c561417b835e97fd
gandh99 <gandh99@hotmail.com>
2020-05-02 18:45:16 +0800

    Added a process.env.NODE_ENV check on client side in order to correctly set the URL at runtime

--- client/src/config/feathers.js
+++ client/src/config/feathers.js
@@ -1,7 +1,14 @@
 import io from 'socket.io-client';
 import feathers from '@feathersjs/client';
 
-const socket = io('http://localhost:5000');
+let socket
+
+if (process.env.NODE_ENV === 'development') {
+  socket = io('http://localhost:5000');
+} else if (process.env.NODE_ENV === 'production') {
+  socket = io('https://feathers-chatroom.herokuapp.com')
+}
+
 const client = feathers();
 
 client.configure(feathers.socketio(socket));
